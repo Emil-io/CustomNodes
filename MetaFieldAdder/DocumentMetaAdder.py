5d77e4ef045471af9765659dc1a9f8629984882b
Emil-io <scheffer.emil@outlook.de>
2024-06-03 10:38:34 +0200

    updated dicsription

--- MetaFieldAdder/DocumentMetaAdder.py
+++ MetaFieldAdder/DocumentMetaAdder.py
@@ -8,21 +8,27 @@ from haystack import Document, component, logging
 @component
 class DocumentMetaAdder:
     """
-    TODO: Description
+    This component adds meta fields to Documents in the indexing pipeline.
+    The meta values are specified in a seperate json file.
+    The name of the json file is the name of the Document the meta data is intended for, plus the ending ".meta.json".
+
+    Example: The name of a pdf file is: "Information.pdf".
+    Then, the name of the json file is "Information.pdf.meta.json".
+    The MetaFieldAdder extracts the meta values from this json file and adds them to the Document.
     """
 
     def __init__(
         self
     ):
         """
-        TODO: Description
+        no arguments
         """
         pass
 
     @component.output_types(documents=List[Document])
     def run(self, documents: List[Document]):
         """
-        TODO: Description
+        It is recommended, to add the component between the FileTypeRouter and the DocumentSplitter.
         """
 
         for doc in documents:
